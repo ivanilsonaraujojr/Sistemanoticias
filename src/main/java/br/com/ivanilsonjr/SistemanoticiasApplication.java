bd447688ab1afdde6ce71df5e11eb29b99c1396b
Ivanilson Junior <ivanilsonaraujojr@gmail.com>
2020-09-17 08:37:12 -0300

    Alterando nome de algumas classes e arquivos

--- src/main/java/br/com/ivanilsonjr/SistemanoticiasApplication.java
+++ src/main/java/br/com/ivanilsonjr/SistemanoticiasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemablogApplication {
+public class SistemanoticiasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemablogApplication.class, args);
+		SpringApplication.run(SistemanoticiasApplication.class, args);
 	}
 
 }
